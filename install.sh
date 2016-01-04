40ec86a8dd3d82430554f4ee24b19452b6af7af4
Jan Schulte <jan.schulte@asquera.de>
2016-01-04 17:07:12 +0100

    Install missing dependencies for Ruby gems

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 apt-get update
 export DEBIAN_FRONTEND=noninteractive #Prevents mysql installer to show set password dialogue
-apt-get install -y git imagemagick mysql-server python-software-properties curl
+apt-get install -y git imagemagick mysql-server python-software-properties curl build-essential libmysqlclient-dev libxslt-dev libxml2-dev zlib1g-dev libmagick++-dev
 mysqladmin -u root password test1234!
 add-apt-repository ppa:brightbox/ruby-ng
 apt-get update
-apt-get install -y ruby2.2
+apt-get install -y ruby2.2 ruby2.2-dev
+gem install bundler
 curl -sL https://deb.nodesource.com/setup_4.x | bash
 apt-get install -y nodejs
 
