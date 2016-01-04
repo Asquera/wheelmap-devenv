86e85b7009c18f0ca5e6c2aff4e4146f92a78e49
1000miles <inthuytion@gmail.com>
2016-01-04 16:33:29 +0100

    Add install instructions for Ruby

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 apt-get update
 export DEBIAN_FRONTEND=noninteractive #Prevents mysql installer to show set password dialogue
-apt-get install -y git imagemagick mysql-server
+apt-get install -y git imagemagick mysql-server python-software-properties
 mysqladmin -u root password test1234!
+add-apt-repository ppa:brightbox/ruby-ng
+apt-get update
+apt-get install -y ruby2.2
