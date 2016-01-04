e71bd9bba1767cc655ed6ae1ccec7e361ed5224a
Jan Schulte <jan.schulte@asquera.de>
2016-01-04 16:39:07 +0100

    Install node 4.2.x

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 apt-get update
 export DEBIAN_FRONTEND=noninteractive #Prevents mysql installer to show set password dialogue
-apt-get install -y git imagemagick mysql-server python-software-properties
+apt-get install -y git imagemagick mysql-server python-software-properties curl
 mysqladmin -u root password test1234!
 add-apt-repository ppa:brightbox/ruby-ng
 apt-get update
 apt-get install -y ruby2.2
+curl -sL https://deb.nodesource.com/setup_4.x | bash
+apt-get install -y nodejs
+
