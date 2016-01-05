19bb55dd5f66a9ae3ca0851d1a759398e4a0aef7
Jan Schulte <jan.schulte@asquera.de>
2016-01-05 12:00:21 +0100

    do not set mysql root password

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 apt-get update
 export DEBIAN_FRONTEND=noninteractive #Prevents mysql installer to show set password dialogue
 apt-get install -y git imagemagick mysql-server python-software-properties curl build-essential libmysqlclient-dev libxslt-dev libxml2-dev zlib1g-dev libmagick++-dev
-mysqladmin -u root password test1234!
 add-apt-repository ppa:brightbox/ruby-ng
 apt-get update
 apt-get install -y ruby2.2 ruby2.2-dev
