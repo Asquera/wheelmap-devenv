b27ab6f6728a6b42e1124ad8ac5edf84259e307e
Jan Schulte <jan.schulte@asquera.de>
2016-08-08 15:29:57 +0200

    Adapt install script for Ubuntu 16.04
    - We use Node LTS from offical package sources
    - We use Ruby 2.3.x from the official package sources

--- install.sh
+++ install.sh
@@ -5,13 +5,8 @@ update-locale LC_ALL=en_US.utf8
 
 apt-get update
 export DEBIAN_FRONTEND=noninteractive #Prevents mysql installer to show set password dialogue
-apt-get install -y git imagemagick mysql-server python-software-properties curl build-essential libmysqlclient-dev libxslt-dev libxml2-dev zlib1g-dev libmagick++-dev vim
-add-apt-repository ppa:brightbox/ruby-ng
-apt-get update
-apt-get install -y ruby2.2 ruby2.2-dev
+apt-get install -y git imagemagick mysql-server python-software-properties curl build-essential libmysqlclient-dev libxslt-dev libxml2-dev zlib1g-dev libmagick++-dev vim ruby ruby-dev nodejs npm
 gem install bundler
-curl -sL https://deb.nodesource.com/setup_4.x | bash
-apt-get install -y nodejs
 add-apt-repository ppa:fcwu-tw/ppa
 apt-get update
 apt-get install -y vim
