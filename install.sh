0e88989f335017768b042876f064633c31b9301c
Jan Schulte <jan.schulte@asquera.de>
2016-08-09 11:50:55 +0200

    We install mysql-5.6 for now
    This is because apparently 5.7 introduces some breaking changes when it
    comes to GEOMETRY objects. To work around these for now we use MySQL 5.6
    and then upgrade to 5.7 when the time has come

--- install.sh
+++ install.sh
@@ -4,12 +4,14 @@
 update-locale LC_ALL=en_US.utf8
 
 apt-get update
+# We need MySQL 5.6 right now, but since it's not in Xenial package repositories, we need to have this one here in place
+add-apt-repository 'deb http://archive.ubuntu.com/ubuntu trusty universe'
 export DEBIAN_FRONTEND=noninteractive #Prevents mysql installer to show set password dialogue
 export MYSQL_ROOT_PASSWORD='root'
 sudo debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
 sudo debconf-set-selections <<< "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 
-apt-get install -y git imagemagick mysql-server python-software-properties curl build-essential libmysqlclient-dev libxslt-dev libxml2-dev zlib1g-dev libmagick++-dev vim ruby ruby-dev nodejs npm
+apt-get install -y git imagemagick mysql-server-5.6 python-software-properties curl build-essential libmysqlclient-dev libxslt-dev libxml2-dev zlib1g-dev libmagick++-dev vim ruby ruby-dev nodejs npm
 gem install bundler
 add-apt-repository ppa:fcwu-tw/ppa
 apt-get update
